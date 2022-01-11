1d0deddb699e90d8f07ba57c2db6c2bcc09880ae
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-11 14:05:53 -0600

    daily-notes first time

---
+++ notes.js
@@ -0,0 +1,11 @@
+const notes = [
+    {
+        id: 1,
+        subject: "I learned about objects and objects nested in arrays. Objects contain properties and values. I think key:value pairs. The property values can be integers, strings, booleans, and one more thing I forgot. You might put objects in an array when you have a list of similar things. All toy objects in one array.",
+        date: "1/11/22",
+        feeling: "Pretty good. Feel a little shakey on vocab, but I practiced outloud to teammates. I also successfully created a github repository and pushed a commit to git hub. Not a total disaster.",
+        timeSpent: "an hour",
+    }
+]
+
+console.log(notes)
